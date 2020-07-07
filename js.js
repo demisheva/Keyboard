22a85ec3b2eddaff543f77b9eaeb4e386346491c
Anna Demisheva <anna@MacBook-Pro-Anna.local>
2020-07-07 16:36:48 +0200

    add new styles

--- js.js
+++ js.js
@@ -5,12 +5,12 @@ function takeElement(domElement) {
     return document.querySelector(domElement);
 }
 
-function t11(event) {
+function activeKey(event) {
     let divKeyCode = takeElement(`.key[data-key-code="${event.keyCode}"]`);
     divKeyCode.classList.add("active-key");
 }
 function removeActiveKey(event) {
     takeElement(".active-key").classList.remove("active-key");
 }
-takeElement('.i-11').onkeydown = t11;
-takeElement('.i-11').onkeyup = removeActiveKey;
+takeElement('.input-txt').onkeydown = activeKey;
+takeElement('.input-txt').onkeyup = removeActiveKey;
